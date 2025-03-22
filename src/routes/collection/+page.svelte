e9eee6016de8c846b4e86698001795d90c9520b2
raymy95 <raymy95@gmail.com>
2025-03-22 10:44:21 +0100

    Initial commit

---
+++ src/routes/collection/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+	<title>Collection</title>
+	<meta name="description" content="Your collection page" />
+</svelte:head>
+
+<div class="text-column">
+	<h1>Collection</h1>
+	<p>Welcome to your collection page!</p>
+</div>
