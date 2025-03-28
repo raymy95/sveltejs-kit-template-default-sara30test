076f2f865f61092340b0a5d4047b86dd79b1e313
raymy95 <raymy95@gmail.com>
2025-03-28 21:01:07 +0100

    netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,9 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true,
-			split:true
-		}),
+		adapter: adapter(),
 		paths: {
 			base: '',
 			relative: false
