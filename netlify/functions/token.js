2368a793c92edea4041a12a4732bd4f6d32f1532
raymy95 <raymy95@gmail.com>
2025-03-28 20:53:58 +0100

    fix2 function

--- netlify/functions/token.js
+++ netlify/functions/token.js
@@ -1,6 +1,6 @@
 exports.handler = async function (request, context) {
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: request.QueryStringParameters["token"] }),
+      body: JSON.stringify({ message: new URL(request.url).searchParams.get("token") }),
     };
   };
